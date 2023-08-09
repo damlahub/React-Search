b53f2a71d278665af2356436217a743030f9d0b9
DamlaOksel <damlaoksel@hotmail.com>
2023-08-09 20:26:07 +0300

    1 | Search Tasarlandı.

---
+++ src/SearchHeader.js
@@ -0,0 +1,12 @@
+function SearchHeader() {
+    return ( 
+        <div className="search-container">
+            <form className="search-form">
+                <label>Ne Arıyorsunuz?</label>
+                <input placeholder="Ara..."/>
+            </form>
+        </div>
+     );
+}
+
+export default SearchHeader;
